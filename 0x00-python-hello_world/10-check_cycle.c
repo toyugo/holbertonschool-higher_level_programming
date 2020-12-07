53ca1d998ea8a9307d3087ed379dcc1217fddaad
Olivier <olivier.guyot.cu@gmail.com>
2020-12-07 19:42:38 +0000

    First commit

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check_cycle
+ * @list: list
+ * Return: 1 true, 0 false
+ **/
+
+int check_cycle(listint_t *list)
+{
+	listint_t *p1;
+	listint_t *p2;
+
+	p1 = list;
+	p2 = list;
+	while (p1->next != NULL && p2->next != NULL && p2->next->next != NULL)
+	{
+		p1 = p1->next;
+		p2 = p2->next;
+		p2 = p2->next;
+		if (p1 == p2)
+			return (1);
+
+	}
+	return (0);
+}
