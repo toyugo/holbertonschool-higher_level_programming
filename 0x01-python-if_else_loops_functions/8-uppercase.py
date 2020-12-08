aaf8f759ba01723eec9c03b08820852e0dd652dc
Olivier <olivier.guyot.cu@gmail.com>
2020-12-08 15:49:23 +0000

    First commit

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def uppercase(str):
+    for i in str:
+        if ord(i) >= ord('a') and ord(i) <= ord('z'):
+            char  = chr(ord(i) - 32)
+        else:
+            char = chr(ord(i))
+        print("{}".format(char), end="")
+    print("")
