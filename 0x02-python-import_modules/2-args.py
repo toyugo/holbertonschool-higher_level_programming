66d486495aa244b26ae27d74ed88b12358a2248e
Olivier <olivier.guyot.cu@gmail.com>
2020-12-10 13:44:40 +0000

    Fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     i = 1
     if lenM - 1 == 0:
         print("{} arguments.".format(lenM - 1))
-    elif lenM -1 == 1:
+    elif lenM - 1 == 1:
         print("1 argument:")
     else:
         print("{} arguments:".format(lenM - 1))
