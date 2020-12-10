8501a048aa0d15236a0090fd8be6682ae3f17032
Olivier <olivier.guyot.cu@gmail.com>
2020-12-10 13:43:01 +0000

    Fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,12 @@ if __name__ == "__main__":
     from sys import argv
     lenM = len(argv)
     i = 1
-    if lenM - 1 != 1:
+    if lenM - 1 == 0:
         print("{} arguments.".format(lenM - 1))
+    elif lenM -1 == 1:
+        print("1 argument:")
     else:
-        print("1 argument")
+        print("{} arguments:".format(lenM - 1))
     while i < lenM:
         print("{}: {}".format(i, argv[i]))
         i += 1
