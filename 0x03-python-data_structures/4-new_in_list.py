07ba9fdfa735e447fdaa0a11e3486a11648f9ed6
Toyugo <2490@holbertonschool.com>
2020-12-15 17:20:17 +0100

    Second

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,1 +1,10 @@
-/#!/usr/bin/python3n/
+#!/usr/bin/python3/
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    if idx > (len(my_list) - 1):
+        return my_list
+    print("test")
+    my_list1 = my_list.copy()
+    my_list1[idx] = element
+    return my_list1
