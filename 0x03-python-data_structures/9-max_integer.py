a5d05aafb44ddb3021f4ce44c35fd6c95f35a078
Toyugo <2490@holbertonschool.com>
2020-12-15 19:04:25 +0100

    first

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,1 +1,11 @@
-/#!/usr/bin/python3n/
+#!/usr/bin/python3/
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return (0)
+    i = 1
+    maxval = my_list[0]
+    while i < len(my_list):
+        if maxval < my_list[i]:
+            maxval = my_list[i]
+        i += 1
+    return (maxval)
