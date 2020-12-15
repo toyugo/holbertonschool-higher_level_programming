1d1d053627c92a5939d9774a16693f43bdd07de8
Toyugo <2490@holbertonschool.com>
2020-12-15 16:58:11 +0100

    Second

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,1 +1,5 @@
-/#!/usr/bin/python3n/
+#!/usr/bin/python3n/
+def print_reversed_list_integer(my_list=[]):
+    i = len(my_list) - 1
+    for i in range(i, -1, -1):
+        print("{}".format(my_list[i]))
