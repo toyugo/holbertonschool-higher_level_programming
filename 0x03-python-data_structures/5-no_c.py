1c776db8c51293d0d599898b37a31b00b21a2776
Toyugo <2490@holbertonschool.com>
2020-12-15 17:30:40 +0100

    Second

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,1 +1,9 @@
-/#!/usr/bin/python3n/
+#!/usr/bin/python3/
+def no_c(my_string):
+    new = ""
+    for i in my_string:
+        if i in "cC":
+            pass
+        else:
+            new += i
+    return new
