04fbef52e6ec5f259c6af49bd583d345771c90bf
Olivier <olivier.guyot.cu@gmail.com>
2020-12-15 21:48:46 +0000

    First commit

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,15 +7,15 @@
 
 int get_max_node(listint_t **head)
 {
-    listint_t *ptr = *head;
-    int i;
-    i = 0;
-    while (ptr)
-    {
-        ptr = ptr->next;
-        i++;
-    }
-    return (i);
+	listint_t *ptr = *head;
+	int i = 0;
+
+	while (ptr)
+	{
+		ptr = ptr->next;
+		i++;
+	}
+	return (i);
 }
 
 /**
@@ -27,18 +27,18 @@ int get_max_node(listint_t **head)
 
 int get_node_index(listint_t *head, int index)
 {
-    listint_t *ptr = head;
-    int i;
-    i = 0;
-    while (i < index)
-    {
-        ptr = ptr->next;
-        i++;
-    }
-    if (ptr)
-        return (ptr->n);
-    else
-        return (-1);
+	listint_t *ptr = head;
+	int i = 0;
+
+	while (i < index)
+	{
+		ptr = ptr->next;
+		i++;
+	}
+	if (ptr)
+		return (ptr->n);
+	else
+		return (-1);
 }
 
 /**
@@ -49,19 +49,19 @@ int get_node_index(listint_t *head, int index)
 
 int is_palindrome(listint_t **head)
 {
-    listint_t *ptrTop = *head;
-    listint_t *ptrBot = *head;
-    int i = 0;
-    int cpTop = 0;
-    int cpBot = 0;
-    cpTop = get_max_node(head) - 1;
+	listint_t *ptrTop = *head;
+	listint_t *ptrBot = *head;
+	int i = 0;
+	int cpTop = 0;
+	int cpBot = 0;
 
-    while (cpTop > cpBot)
-    {
-        if (get_node_index(ptrTop, cpTop) != get_node_index(ptrBot, cpBot))
-            return (-1);
-        cpTop--;
-        cpBot++;
-    }
-    return (1);
+	cpTop = get_max_node(head) - 1;
+	while (cpTop > cpBot)
+	{
+		if (get_node_index(ptrTop, cpTop) != get_node_index(ptrBot, cpBot))
+			return (-1);
+		cpTop--;
+		cpBot++;
+	}
+	return (1);
 }
