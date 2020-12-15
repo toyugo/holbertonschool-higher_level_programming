dffd6d412c2533f98ae72155159c45250063b46f
Toyugo <2490@holbertonschool.com>
2020-12-15 18:41:24 +0100

    second

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3/
 def multiple_returns(sentence):
+    if sentence is None:
+        return None
     lenght = len(sentence)
     firstchar = sentence[0]
     return lenght, firstchar
