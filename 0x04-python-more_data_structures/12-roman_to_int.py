57f1c57d6e4b04e8ac496c7615171aa858db287d
Olivier <olivier.guyot.cu@gmail.com>
2020-12-17 20:40:41 +0000

    Fix

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_dict = { "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500,"M": 1000 }
+    sizeidx = len(roman_string) - 1
+    sum = 0
+    i = 0
+    while i <= sizeidx:
+        if roman_string[i] in roman_dict:
+            n = roman_dict[roman_string[i]]
+            if (i + 1) <= sizeidx:
+                n1 =  roman_dict[roman_string[i + 1]]
+                if n >= n1:
+                    sum += n
+                else:
+                    sum -= n
+                    i += 1
+                    sum += n1
+            else:
+                sum += roman_dict[roman_string[i]]
+        else:
+            return None
+        i += 1
+    return (sum)
