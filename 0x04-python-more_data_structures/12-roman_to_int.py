26a41e7a976236f48facf6289ad80e30ba87c854
Olivier <olivier.guyot.cu@gmail.com>
2020-12-17 20:50:20 +0000

    Fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,6 +4,8 @@ def roman_to_int(roman_string):
         "I": 1, "V": 5, "X": 10,
         "L": 50, "C": 100, "D": 500, "M": 1000
     }
+    if not isinstance(roman_string, str):
+        return 0
     sizeidx = len(roman_string) - 1
     sum = 0
     i = 0
