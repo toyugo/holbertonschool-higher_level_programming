a7896ec9b97ac8f65649a571d3e0b78ef8f830a0
Olivier <olivier.guyot.cu@gmail.com>
2020-12-17 16:45:51 +0000

    Fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,16 @@
-#!/usr/bin/python3/
+#!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary == None:
         return None
-    valMax = max(a_dictionary, key=a_dictionary.get)
-    return valMax
+    new = a_dictionary.copy()
+    new = new.values()
+    new = list(new)
+    new.sort(reverse=True)
+    maxVal = new[0]
+    for i, j in a_dictionary.items():
+        if j == maxVal:
+            print(i)
+
+
+a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+best_score(a_dictionary)
