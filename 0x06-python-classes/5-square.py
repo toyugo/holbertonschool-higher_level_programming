7614409d72bb7624da46a7f2dc15f0f70c6a54cf
Olivier <olivier.guyot.cu@gmail.com>
2021-01-05 17:37:38 +0000

    Fix

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+"""Square module"""
 
 
 class Square:
+    """Square class"""
     def __init__(self, size=0):
+        """Init
+        Args:
+            size (int): size
+        """
         self.__size = size
 
     @property
