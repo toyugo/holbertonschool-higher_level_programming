611833575496a8be9357de712358176cf48a935b
Olivier <olivier.guyot.cu@gmail.com>
2021-01-05 22:48:10 +0000

    Fix

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0, position=(0, 0)):
+        """Init
+        Args:
+            size (int): size
+            position (tuple): position
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """getter"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """setter"""
+        if isinstance(value, tuple) and len(value) == 2 \
+            and isinstance(value[0], int) and isinstance(value[1], int) \
+                and value[0] >= 0 and value[1] >= 0:
+                    self.__position = value
+        else:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def area(self):
+        """calculate square"""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """Print square"""
+        if (self.__size == 0):
+            print("")
+        else:
+            for i in range(0, self.__position[1]):
+                print("")
+            for i in range(0, self.__size):
+                for k in range(0, self.__position[0]):
+                    print(" ", end='')
+                # for j in range(0, self.size):
+                for j in range(0, self.__size):
+                    print("#", end='')
+                print("")
+
+    def __str__(self):
+        """Print replace"""
+        tmp_str = ""
+        cp = 0
+        for i in range(0, self.__position[1]):
+            tmp_str = "\n"
+        for i in range(0, self.__size):
+            for k in range(0, self.__position[0]):
+                tmp_str = tmp_str + " "
+            for j in range(0, self.__size):
+                tmp_str = tmp_str + "#"
+            cp = cp + 1
+            if cp != self.__size:
+                tmp_str = tmp_str + "\n"
+        return tmp_str
