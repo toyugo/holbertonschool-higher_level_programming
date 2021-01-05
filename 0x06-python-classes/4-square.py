bbc4ea7fb9bb7110debbfd492d77839ae44f8c08
Olivier <olivier.guyot.cu@gmail.com>
2021-01-05 16:53:47 +0000

    Fix

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -20,7 +20,7 @@ class Square:
             self.__size = value
         else:
             raise TypeError("size must be an integer")
-        if value <= 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
 
     def area(self):
