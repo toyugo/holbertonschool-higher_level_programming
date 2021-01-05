f15daf4a262ce10111faa6985c901a96f4a18417
Olivier <olivier.guyot.cu@gmail.com>
2021-01-05 17:36:11 +0000

    Fix

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter"""
+        if isinstance(value, int):
+            self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """calculate square"""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """Print square"""
+        if (self.size == 0):
+            print("")
+        for i in range(0, self.size):
+            for j in range(0, self.size):
+                print("#", end='')
+            print("")
