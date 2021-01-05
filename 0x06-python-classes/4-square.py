e5640719dd76e8f0c2f423cf8f15caf855ec6f93
Olivier <olivier.guyot.cu@gmail.com>
2021-01-05 16:52:37 +0000

    Fix

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        """Init"""
+        self.size = size
+
+    @property
+    def size(self):
+        """Getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter"""
+        if isinstance(value, int):
+            self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+        if value <= 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Area calculation"""
+        return (self.__size * self.__size)
