5c5074aa4a4a73bb02513e985822414189388cf6
Olivier <olivier.guyot.cu@gmail.com>
2021-01-11 21:04:36 +0000

    Fix

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Module rectangle """
+
+
+class Rectangle:
+    """ Rectangle """
+    def __init__(self, size=0):
+        pass
---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" Module rectangle """
+
+
+class Rectangle:
+    """ Rectangle """
+    def __init__(self, width=0, height=0):
+        """ Init """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return (self.__width + self.__height) * 2
