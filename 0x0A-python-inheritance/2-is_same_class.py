b9a96f34aea1487f0897f4f47e95f5eb548c535a
Olivier <olivier.guyot.cu@gmail.com>
2021-01-19 19:47:02 +0000

    Fix

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module create the look up function
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    True if the object is exactly an 
+    instance of the specified class ; otherwise False.
+    """
+    return type(obj is a_class)
