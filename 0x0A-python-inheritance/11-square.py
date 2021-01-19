10110df0971111332744ef49560638fabec8ed7f
Olivier <olivier.guyot.cu@gmail.com>
2021-01-19 21:26:04 +0000

    Fix

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""
-This module create the look up function
-"""
-
+"""Defines a Rectangle subclass Square."""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """ a square."""
+    """a square."""
+
     def __init__(self, size):
-        """ init """
+        """ Init  """
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def __str__(self):
+        """ print """
+        return "[Square] {}/{}".format(self.__size, self.__size)
