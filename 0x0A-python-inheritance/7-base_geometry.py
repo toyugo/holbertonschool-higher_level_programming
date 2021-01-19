18b17e8d2e43d8a157c080bc3e2f1102437b7b5b
Olivier <olivier.guyot.cu@gmail.com>
2021-01-19 20:48:41 +0000

    Fix

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -12,7 +12,7 @@ class BaseGeometry():
 
     def integer_validator(self, name, value):
         """ integer_validator """
-        if isinstance(value, int)is False:
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -12,11 +12,10 @@ class BaseGeometry():
 
     def integer_validator(self, name, value):
         """ integer_validator """
-        if isinstance(value, int)is False:
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-
 
 class Rectangle(BaseGeometry):
     def __init__(self, width, height):
