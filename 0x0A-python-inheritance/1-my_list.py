85c0737e24c8dfb01a1179ec3ec7a9950a7e8eec
Olivier <olivier.guyot.cu@gmail.com>
2021-01-19 18:08:58 +0000

    Fix

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module create the look up function
+"""
+
+class MyList(list):
+    " Simple List "
+    def print_sorted(self):
+        """return the list attribute"""
+        list1 = sorted(self.copy())
+        print(list1)
