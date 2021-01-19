b9102df54d7a5c9d869f74c36b0a135d25cebfd1
Olivier <olivier.guyot.cu@gmail.com>
2021-01-19 20:43:12 +0000

    Fix

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module create the look up function
+"""
+
+
+class BaseGeometry():
+    """ Base Geometry """
+    def area(self):
+        """ Area """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ integer_validator """
+        if isinstance(value, int)is False:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        """ Rectangle """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
