3eebf4d360f7db1bebc606441bd8368fb899dd4b
Olivier <olivier.guyot.cu@gmail.com>
2021-01-20 15:51:32 +0000

    Fix

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,6 +8,6 @@ def write_file(filename="", text=""):
     file (UTF8) and returns the number of characters written
     """
     cp = 0
-    with open(filename, mode="w", encoding="utf8") as f:
+    with open(filename, mode="w") as f:
         f.write(text)
     return(len(text))
