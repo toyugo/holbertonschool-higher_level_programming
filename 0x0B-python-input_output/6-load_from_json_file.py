3f2c32de4972c20f1f2183d80f0dc3ea24927076
Olivier <olivier.guyot.cu@gmail.com>
2021-01-20 20:42:44 +0000

    Fix

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,6 +6,9 @@ def load_from_json_file(filename):
     """
     function that creates an Object from a “JSON file”
     """
+    mytxt = ""
     with open(filename, mode="r") as f:
-        return(json.load(f))
+        for line in f.readlines():
+            mytxt = mytxt + line
+    return json.loads(mytxt)
 
