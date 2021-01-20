c10757f18d299e57151184bfb3c75006e5b39d3e
Olivier <olivier.guyot.cu@gmail.com>
2021-01-20 22:59:11 +0000

    Fix

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Triangle of pascal """
+
+
+def pascal_triangle(n):
+    """Create a function that return a pascal triangle"""
+    ls = []
+    if n <= 0:
+        return ls
