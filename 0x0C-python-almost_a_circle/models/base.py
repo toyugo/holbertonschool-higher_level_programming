0fcf9a3d0eaf4c950884d9aa74efc8be16fdc154
Olivier <olivier.guyot.cu@gmail.com>
2021-01-26 16:26:19 +0000

    Fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -47,10 +47,9 @@ class Base():
     @classmethod
     def create(cls, **dictionary):
         """ Definition of the function"""
-        if dictionary and dictionary != {}:
-            if cls.__name__ == "Rectangle":
-                new = cls(1, 1)
-            elif cls.__name__ == "Square":
-                new = cls(1)
-            new.update(**dictionary)
-            return new
+        if cls.__name__ == "Rectangle":
+            new = cls(1, 1)
+        elif cls.__name__ == "Square":
+            new = cls(1)
+        new.update(**dictionary)
+        return new
