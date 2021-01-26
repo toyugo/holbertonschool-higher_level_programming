a0a3cbc2bae45a8186aa1e8a0e4c20477353af83
Olivier <olivier.guyot.cu@gmail.com>
2021-01-26 17:05:29 +0000

    Fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -3,38 +3,8 @@
 from models.base import Base
 
 
-def checkType(value, attr_1):
-    """ Definition of the function """
-    if type(value) is not int:
-        raise TypeError("{} must be an integer".format(attr_1))
-    else:
-        return True
-
-
-def checkValue(value, attr_1):
-    """ Definition of the function """
-    if value < 0:
-        raise ValueError("{} must be >= 0".format(attr_1))
-    else:
-        return True
-
-
-def checkValueHeight(value, attr_1):
-    """ Definition of the function """
-    if value <= 0:
-        raise ValueError("{} must be > 0".format(attr_1))
-    else:
-        return True
-
-
 class Rectangle(Base):
-    """ Definition of the function
-    Private instance attributes, each with its own public getter and setter:
-    __width -> width
-    __height -> height
-    __x -> x
-    __y -> y
-    """
+    """ Definition of the function """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Definition of the function """
@@ -52,7 +22,6 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """ Definition of the function """
-        # if checkType(value, "width") and checkValueHeight(value, "width"):
         self.__width = value
 
     @property
@@ -87,44 +56,3 @@ class Rectangle(Base):
         """ Definition of the function """
         # if checkType(value, "y") and checkValue(value, "y"):
         self.__y = value
-
-    def area(self):
-        """ Definition of the function """
-        return self.__width * self.__height
-
-    def display(self):
-        """Print a rectangle"""
-        for k in range(0, self.__y):
-            print()
-        for i in range(0, self.__height):
-            for l in range(0, self.__x):
-                print(" ", end="")
-            for j in range(0, self.__width):
-                print("#", end="")
-            print()
-
-    def __str__(self):
-        """ Str overide """
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
-                                                       self.__x,
-                                                       self.__y,
-                                                       self.__width,
-                                                       self.__height)
-
-    def update(self, *args, **kwargs):
-        """ Definition of the function """
-        ls = ["id", "width", "height", "x", "y"]
-        if args and len(args) != 0:
-            for i in range(0, len(args)):
-                setattr(self, ls[i], args[i])
-        elif kwargs and len(kwargs) != 0:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-
-    def to_dictionary(self):
-        """ Definition of the function """
-        return {"id": self.id,
-                "width": self.__width,
-                "height": self.__height,
-                "x": self.__x,
-                "y": self.__y}
