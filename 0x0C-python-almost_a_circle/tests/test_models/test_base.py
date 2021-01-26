897cb56ba4728a10a20da128d9b88ccc28781fe8
Olivier <olivier.guyot.cu@gmail.com>
2021-01-26 17:50:18 +0000

    Fix

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -7,8 +7,14 @@ import unittest
 
 class TestBase(unittest.TestCase):
     """ Unit test Base class """
-
+    def setUp(self):
+        self.r1 = Rectangle(10, 2)
+        self.r2 = Rectangle(2, 10)
+        self.r3 = Rectangle(10, 2, 0, 0, 12)
     def test_id(self):
-        self.assertEqual(1, Base().id)
-        self.assertEqual(15, Base(15).id)
-        self.assertEqual(2, self.Base(None)
+        # compare argument 1 avec argument 2 et verifie le resultat)
+        self.assertEqual(self.r1.id, 1)
+        print(self.r2.id)
+        self.assertEqual(self.r2.id, 2)
+        print(self.r3.id, 12)
+        
