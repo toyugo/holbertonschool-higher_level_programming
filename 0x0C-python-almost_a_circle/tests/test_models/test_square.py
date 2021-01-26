d30bba35590f206a27c56a5271db6747d92f3ce4
Olivier <olivier.guyot.cu@gmail.com>
2021-01-26 19:42:18 +0000

    Fix

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,32 @@
+import unittest
+from models.base import Base
+from models.square import Square
+
+class TestRectangle(unittest.TestCase):
+    """ Unit test Base class """
+    # def __init__(self, size, x=0, y=0, id=None)::
+    # attr = ["width", "height", "x", "y"]
+    def setUp(self):
+        # print(Rectangle.__base__.__dict__)
+        Square._Base__nb_objects = 0
+    def test_size_negatif(self):
+        with self.assertRaises(ValueError) as cm:
+            Square(-1, 10, 10)
+
+        self.assertEqual(str(cm.exception), "width must be > 0")
+
+    def test_x_negatif(self):
+        with self.assertRaises(ValueError) as cm:
+            Square(10, -2, 10)
+        self.assertEqual(str(cm.exception), "x must be >= 0")
+
+    def test_y_negatif(self):
+        with self.assertRaises(ValueError) as cm:
+            Square(10, 10, -2)
+        self.assertEqual(str(cm.exception), "y must be >= 0")
+
+    # ZERO
+    def test_size_zero(self):
+        with self.assertRaises(ValueError) as cm:
+            Square(0, 10, 10)
+        self.assertEqual(str(cm.exception), "width must be > 0")
