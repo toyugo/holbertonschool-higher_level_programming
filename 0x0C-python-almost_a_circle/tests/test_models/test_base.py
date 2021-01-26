d9e1e3f00227ba0d655efb466b7a03ec0cde9b8a
Olivier <olivier.guyot.cu@gmail.com>
2021-01-26 11:09:54 +0000

    Fix

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,15 +1,23 @@
-from unittest import TestCase
+#!/usr/bin/python3
+""" Project: Python Almost a Circle """
 from models.base import Base
 from models.rectangle import Rectangle
-from models.square import Square
+import unittest
 
 
-class TestBaseID(TestCase):
-    """Test Base id."""
+class TestBase(unittest.TestCase):
+    """ Unit test Base class """
 
     def setUp(self):
         Base._Base__nb_objects = 0
+        self.base_1 = Base()
+        self.base_2 = Base(15)
+        self.base_3 = Base(None)
 
     def test_id(self):
-        self.assertEqual(Base(15).id,15)
-        self.assertEqual(Base().id, 1)
+        self.assertEqual(1, self.base_1.id)
+        self.assertEqual(15, self.base_2.id)
+        self.assertEqual(2, self.base_3.id)
+
+    def test_nb_objects(self):
+        self.assertEqual(2, Base._Base__nb_objects)
