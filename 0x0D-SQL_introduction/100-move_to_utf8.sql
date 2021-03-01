e927d47bffad4a0db22d89b5514fe96cf45a09b0
Your Name <you@example.com>
2021-03-01 22:04:57 +0000

    Fix version

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,9 +1,10 @@
 -- Lists all records
-ALTER DATABASE
-    CHARACTER SET = utf8mb4
-    COLLATE = utf8mb4_unicode_ci;
 ALTER TABLE
     first_table
     CONVERT TO CHARACTER 
     SET utf8mb4
     COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE
+    hbtn_0c_0
+    CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci;
