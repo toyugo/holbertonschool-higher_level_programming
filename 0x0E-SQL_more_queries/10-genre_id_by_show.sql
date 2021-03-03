589d52e4f4283a014d0a3d3452d5da64f55a5e8a
Your Name <you@example.com>
2021-03-03 21:46:55 +0000

    Fix version

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,6 +2,6 @@
 -- Desc
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres
+JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
