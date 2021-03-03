5b0a71841707caa1cc9e63aba2f6ea1da0432131
Your Name <you@example.com>
2021-03-03 18:42:56 +0000

    Fix version

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- Desc
+-- Desc
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    state_id INT NOT NULL,
+    FOREIGN KEY state_id REFERENCES states(id),
+    name VARCHAR(256) NOT NULL
+)
