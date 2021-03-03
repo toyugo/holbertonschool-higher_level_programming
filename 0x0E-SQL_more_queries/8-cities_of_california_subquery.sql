e642ce96fcff049b466719c7cc2c9c944efca9d7
Your Name <you@example.com>
2021-03-03 20:23:07 +0000

    Fix version

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,9 @@
 -- Desc
 
-SELECT cities.id, cities.name FROM cities
-WHERE state_id IN (SELECT id FROM states WHERE name = "California")
+-- SELECT cities.id, cities.name FROM cities
+-- WHERE state_id IN (SELECT id FROM states WHERE name = "California")
+-- ORDER BY cities.id ASC;
+SELECT cities.id, cities.name FROM states, cities
+WHERE states.id = cities.state_id
+AND states.name = 'California'
 ORDER BY cities.id ASC;
