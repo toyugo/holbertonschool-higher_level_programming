35e403b73b1d8eee50898571661c1d197f6aeffd
Your Name <you@example.com>
2021-03-03 20:19:21 +0000

    Fix version

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- Desc
 -- Desc
 USE hbtn_0d_usa;
-SELECT cities.id, cities.name FROM states, cities
-WHERE states.id = cities.state_id
+SELECT cities.id, cities.name FROM cities
+WHERE state_id IN (SELECT id FROM states WHERE name = "California")
 ORDER BY cities.id ASC;
