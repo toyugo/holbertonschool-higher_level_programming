e008a15231930601d10a99d64697ebe12024a62e
Your Name <you@example.com>
2021-03-03 18:38:13 +0000

    Fix version

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- Desc
+-- Desc
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT UNIQUE PRIMARY KEY NOT NULL,
+    name VARCHAR(256) NOT NULL
+);
