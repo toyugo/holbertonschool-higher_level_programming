61758f66a42b59494a9178fe23c3f4c5a397564b
Your Name <you@example.com>
2021-03-03 22:17:19 +0000

    Fix version

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,10 @@
+-- Desc
+-- Desc
+SELECT tv_genres.name AS genre, COUNT(tv_shows.title) as number_of_shows
+FROM tv_genres
+LEFT JOIN tv_show_genres
+ON tv_show_genres.genre_id = tv_genres.id
+LEFT JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
