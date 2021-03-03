bf59cfff26df183e38a531b91d941dff02e2128a
Your Name <you@example.com>
2021-03-03 22:49:39 +0000

    Fix version

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,12 @@
+-- Desc
+-- Desc
+SELECT tv_genres.name AS "name", SUM(tv_show_ratings.rate) AS "rating" 
+FROM tv_show_ratings 
+JOIN tv_shows 
+ON tv_shows.id = tv_show_ratings.show_id 
+JOIN tv_show_genres 
+ON tv_show_genres.show_id = tv_shows.id 
+JOIN tv_genres 
+ON tv_genres.id = tv_show_genres.genre_id 
+GROUP BY name 
+ORDER BY rating DESC;
