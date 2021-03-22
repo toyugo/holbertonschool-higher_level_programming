09a4c43e2a8f43da13e5266032d0be9fd8744527
Your Name <you@example.com>
2021-03-22 21:51:47 +0000

    Fix version

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+function factorial (n) {
+  let tot = 0;
+
+  if (n === 0) {
+    return (1);
+  }
+  tot = n * factorial(n - 1);
+  return (tot);
+}
+console.log(factorial(process.argv[2]));
