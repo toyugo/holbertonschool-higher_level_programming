2723bdd844b8e586af27ff640fb525424cfa5960
Your Name <you@example.com>
2021-03-22 22:36:41 +0000

    Fix version

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,15 +4,17 @@ let noduplicate = [];
 let i = 2;
 let res = 0;
 
-for (i = 2; i < process.argv.length; i++) {
-  nb.push(parseInt(process.argv[i], 10));
-}
-noduplicate = Array.from(new Set(nb));
-res = Math.max(...noduplicate);
-for (i = 0; i < noduplicate.length; i++) {
-  if (noduplicate[i] === res) {
-    noduplicate.splice(i, 1);
+if (process.argv.length > 3) {
+  for (i = 2; i < process.argv.length; i++) {
+    nb.push(parseInt(process.argv[i], 10));
   }
+  noduplicate = Array.from(new Set(nb));
+  res = Math.max(...noduplicate);
+  for (i = 0; i < noduplicate.length; i++) {
+    if (noduplicate[i] === res) {
+      noduplicate.splice(i, 1);
+    }
+  }
+  res = Math.max(...noduplicate);
 }
-res = Math.max(...noduplicate);
 console.log(res);
