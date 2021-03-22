7d20fa9d56aa5091043263133bfa66843c486f45
Your Name <you@example.com>
2021-03-22 22:33:27 +0000

    Fix version

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const nb = [];
+let noduplicate = [];
+let i = 2;
+let res = 0;
+
+for (i = 2; i < process.argv.length; i++) {
+  nb.push(parseInt(process.argv[i], 10));
+}
+noduplicate = Array.from(new Set(nb));
+res = Math.max(...noduplicate);
+for (i = 0; i < noduplicate.length; i++) {
+  if (noduplicate[i] === res) {
+    noduplicate.splice(i, 1);
+  }
+}
+res = Math.max(...noduplicate);
+console.log(res);
