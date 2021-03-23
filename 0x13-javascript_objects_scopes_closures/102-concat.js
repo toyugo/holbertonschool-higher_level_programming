184c2ca12ca2be01a4861173b42047c7dd1cf413
Your Name <you@example.com>
2021-03-23 22:04:50 +0000

    Fix version

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,6 +4,6 @@ let output = fs.readFileSync(process.argv[2], 'utf-8');
 const output2 = fs.readFileSync(process.argv[3], 'utf-8');
 
 output = output + '\n' + output2;
-fs.writeFile(process.argv[3], output, function (err) {
+fs.writeFile(process.argv[4], output, function (err) {
   if (err) throw err;
 });
