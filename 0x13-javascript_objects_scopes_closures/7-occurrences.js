d44ad58b6c62245e823f1363d2a11d4edae6dead
Your Name <you@example.com>
2021-03-23 16:36:31 +0000

    Fix version

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let i = 0;
+  let cp = 0;
+
+  for (i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      cp++;
+    }
+  }
+  return (cp);
+};
