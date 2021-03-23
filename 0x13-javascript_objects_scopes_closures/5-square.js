9ba09ec9bf7f2f091317d0213d5fd1b492f1fe48
Your Name <you@example.com>
2021-03-23 16:16:59 +0000

    Fix version

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,1 +1,40 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let res = '';
+    for (let i = 0; i < this.height; i++) {
+      res = '';
+      for (let j = 0; j < this.width; j++) {
+        res = res + 'X';
+      }
+      console.log(res);
+    }
+  }
+
+  rotate () {
+    const tmp = this.height;
+    this.height = this.width;
+    this.width = tmp;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+
+module.exports = Rectangle;
+module.exports = Square;
--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,1 @@
 #!/usr/bin/node
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-}
