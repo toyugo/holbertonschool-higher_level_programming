c48554b62154f4b555cb3cbca0291985125e773b
Your Name <you@example.com>
2021-03-23 16:05:33 +0000

    Fix version

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let res = '';
+    for (let i = 0; i < this.height; i++) {
+      res = '';
+      for (let j = 0; j < this.width; j++) {
+        res = res + 'X';
+      }
+      console.log(res);
+    }
+  }
+}
+
+module.exports = Rectangle;
