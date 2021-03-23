f570317d0c9f336b14331170335b06c2f04133be
Your Name <you@example.com>
2021-03-23 16:42:00 +0000

    Fix version

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+exports.esrever = function (list) {
+  let i = 0;
+  const res = [];
+
+  for (i = list.length - 1; i >= 0; i--) {
+    res.push(list[i]);
+  }
+  return (res);
+};
