15730741d8671e5faefc0423fa2f5968a2209e48
Toyugo <olivier.guyot@holbertonschool.com>
2021-03-25 22:45:37 +0000

    fix

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    mysql_cmd = "SELECT cities.id, cities.name, states.name\
+                 FROM cities\
+                 INNER JOIN states ON\
+                 cities.state_id = states.id;"
+    cursor.execute(mysql_cmd)
+    data = cursor.fetchall()
+    for row in data:
+        print(row)
