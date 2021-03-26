2c2741d2e52f794522a2320bcc756f678d7d91e3
Toyugo <olivier.guyot@holbertonschool.com>
2021-03-26 10:15:36 +0000

    fix

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy import create_engine, Table
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+import sys
+
+connInfo = 'mysql+mysqldb://{}:{}@localhost/{}'
+engine = create_engine(connInfo.format(sys.argv[1],
+                                       sys.argv[2],
+                                       sys.argv[3]),
+                       pool_pre_ping=True)
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+
+session = Session()
+states = session.query(State).order_by(State.id.asc()).all()
+print(str(states[0].id) + ": " + states[0].name)
