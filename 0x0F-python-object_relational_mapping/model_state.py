9976e8838d57da75a5d2b7933462256dc81a0f72
Toyugo <olivier.guyot@holbertonschool.com>
2021-03-26 08:43:05 +0000

    fix

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """ State definition """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name =  Column(String(128), nullable=False)
