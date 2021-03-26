af73b45605fe3523d63ca06af649ed7c647721dc
Toyugo <olivier.guyot@holbertonschool.com>
2021-03-26 16:26:03 +0000

    fix

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy import create_engine, Table
+from sqlalchemy import MetaData
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+from sqlalchemy import text
+import sys
+
+connInfo = 'mysql+mysqldb://{}:{}@localhost/{}'
+engine = create_engine(connInfo.format(sys.argv[1],
+                                       sys.argv[2],
+                                       sys.argv[3]),
+                       pool_pre_ping=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+newRow = State(name="Louisiana")
+session.add(newRow)
+session.commit()
+print(newRow.id)
+session.close()
