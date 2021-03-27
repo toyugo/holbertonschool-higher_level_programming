6fef6ee701a81ebf264c0838fbb12e083133a987
Toyugo <olivier.guyot@holbertonschool.com>
2021-03-27 10:29:22 +0000

    fix

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,10 +13,9 @@ if __name__ == "__main__":
                                         sys.argv[2],
                                         sys.argv[3]),
                         pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
     Base = declarative_base()
 
-    session = Session()
+    session = Session(engine)
     # states = session.query(State).order_by(State.id.asc()).all()
     states = session.query(State)
     for row in states:
