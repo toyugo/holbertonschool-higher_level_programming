b4a4921b6489cfe8cfe5a8bd3209ae4b705fc7e3
Toyugo <olivier.guyot@holbertonschool.com>
2021-03-28 11:02:53 +0000

    fix

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python3
 """ Description module """
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy import create_engine, Table
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from sqlalchemy import text
+from sqlalchemy import update
+import sys
+
+if __name__ == "__main__":
+    connInfo = 'mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine(
+                            connInfo.format(sys.argv[1],
+                                            sys.argv[2],
+                                            sys.argv[3]),
+                            pool_pre_ping=True)
+    session = Session(engine)
+    Base = declarative_base()
+    query = session.query(State).filter(State.name.ilike("%a%")).all()
+    for item in list_to_del:
+        session.delete(item)
+    session.commit()
