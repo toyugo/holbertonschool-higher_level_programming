cf8e29db230937d7c1791c53ce502f26df8b484b
Toyugo <olivier.guyot@holbertonschool.com>
2021-03-28 11:16:31 +0000

    fix

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Description module """
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """ State definition """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Description module """
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """description"""
+    __tablename__ = 'cities'
+    id = Column(Integer,
+                autoincrement="auto",
+                nullable=False,
+                primary_key=True,
+                unique=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer,
+                      ForeignKey('states.id'),
+                      nullable=False)
