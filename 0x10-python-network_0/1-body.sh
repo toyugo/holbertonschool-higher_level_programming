0de487e3f6e378350e16c21a13222ade8fd75750
O <olivier.guyot@holbertonschool.fr>
2021-04-08 10:49:57 +0000

    fix

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Get the byte size
-if [ "$(curl -sL -w "%{http_code}" -I "$1" -o /bin/null)" -eq 200 ]; then curl -sL "$1" ; fi
+# if [ "$(curl -sL -w "%{http_code}" -I "$1" -o /bin/null)" -eq 200 ]; then curl -sL "$1" ; fi
+[ "$(curl -sL -w "%{http_code}" -I "$1" -o /bin/null)" -eq 200 ] && curl -sL "$1"
