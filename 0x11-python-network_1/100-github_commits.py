7b22689009bd0d5f24b0e2a8a9b153d0bcfe2732
O <olivier.guyot@holbertonschool.fr>
2021-04-09 15:57:18 +0000

    fix

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" Python script that lists list 10 """
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    username = argv[1]
+    repo = argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits?per_page=10".format(username, repo)
+    request = requests.get(url)
+    data = request.json()
+    for info in data:
+        print("{}: {}".format(info.get("sha"),
+                              info.get("commit").get("author").get("name")))
