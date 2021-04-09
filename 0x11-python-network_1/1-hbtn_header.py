d797a697485df6d2aee7dd69e06b9d47223f8c0a
O <olivier.guyot@holbertonschool.fr>
2021-04-09 12:23:30 +0000

    fix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" Module  to get url response """
+import urllib.request as ur
+import sys
+
+if __name__ == "__main__":
+    with ur.urlopen(str(sys.argv[1])) as resp:
+        a = resp.info()
+    print("{}".format(a['X-Request-Id']))
+    
