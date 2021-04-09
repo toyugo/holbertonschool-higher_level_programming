21324e36d9decc7f54b8c444411bca872a40c7c6
O <olivier.guyot@holbertonschool.fr>
2021-04-09 12:35:17 +0000

    fix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,6 +4,6 @@ import urllib.request
 import sys
 
 if __name__ == "__main__":
-    with urllib.request.urlopen(str(sys.argv[1])) as resp:
+    with urllib.request.urlopen(sys.argv[1]) as resp:
         a = resp.info()
     print("{}".format(a['X-Request-Id']))
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" Module  to get url response """
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    data = urllib.parse.urlencode({"email": sys.argv[2]})
+    data = data.encode('ascii')
+    url = sys.argv[1]
+    with open(urllib.request.urlopen(url, data)):
+        print("Your email is : {}".format(response.info['email']))
