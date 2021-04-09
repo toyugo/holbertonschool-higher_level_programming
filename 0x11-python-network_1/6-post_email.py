33aeb4ac657b3bc5b7c3f20a7a6fd9a918599c62
O <olivier.guyot@holbertonschool.fr>
2021-04-09 13:43:09 +0000

    fix

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" Fetches POST """
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    url = argv[1]
+    email = argv[2]
+    params = {'email': email}
+    resp = requests.post(url, params)
+    print(resp.text)
