126c731a80a7cfd9b522f0b23c9a07c01adca4d3
O <olivier.guyot@holbertonschool.fr>
2021-04-09 15:41:29 +0000

    fix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+""" connect to git api """
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    if len(argv) >= 1:
+        username = argv[1]
+        api_token = argv[2]
+        session = requests.Session()
+        session.headers['Authorization'] = 'token %s' % api_token
+        url = 'https://api.github.com/repos/toyugo/MyDocument'
+        resp = session.get(url)
+        if resp.status_code == 200:
+            json = resp.json()
+            print (json.get("id"))
+        else:
+            print (None)
