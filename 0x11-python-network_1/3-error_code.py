68545f85fcc7166374510137c8bddbfa8472fab5
O <olivier.guyot@holbertonschool.fr>
2021-04-09 12:51:39 +0000

    fix

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,8 +6,6 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    data = urllib.parse.urlencode({"email": sys.argv[2]})
-    data = data.encode('ascii')
     try:
         with urllib.request.urlopen(sys.argv[1]) as resp:
             a = resp.read()
