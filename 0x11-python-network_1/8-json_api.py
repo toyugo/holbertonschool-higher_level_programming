c93ed8f6c89459d548878c11fc8955e892f6875d
O <olivier.guyot@holbertonschool.fr>
2021-04-09 14:56:49 +0000

    fix

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+    sends a POST request.
+"""
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    if len(argv) > 1:
+        letter = argv[1]
+    else:
+        letter = ""
+    params = {'q': letter}
+    url = 'http://0.0.0.0:5000/search_user'
+    resp = requests.post(url, params)
+    try:
+        json = resp.json()
+        if json:
+            print('[{}] {}'.format(json['id'], json['name']))
+        else:
+            print('No result')
+    except:
+        print('Not a valid JSON')
