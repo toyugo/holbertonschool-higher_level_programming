b6b45eaf3221c226a14e41f18e33d155daeb81cd
O <olivier.guyot@holbertonschool.fr>
2021-04-09 15:58:59 +0000

    fix

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,10 +6,10 @@ from sys import argv
 if __name__ == "__main__":
     username = argv[1]
     repo = argv[2]
-    url = "https://api.github.com/repos/{}/{}/commits?per_page=10".format(username, repo)
+    url = "https://api.github.com/repos/{}/{}/commits?per_page=10"\
+        .format(username, repo)
     request = requests.get(url)
     data = request.json()
     for info in data:
         print("{}: {}".format(info.get("sha"),
                               info.get("commit").get("author").get("name")))
-
