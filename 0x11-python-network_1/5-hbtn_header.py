a0e02524a4f5aaca422f48c6c4f89ee9a70258ae
O <olivier.guyot@holbertonschool.fr>
2021-04-09 14:06:01 +0000

    fix

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -12,4 +12,5 @@ if __name__ == '__main__':
         pass
     else:
         headtext = resp.headers
-        print(headtext['X-Request-Id'])
+        if 'X-Request-Id' in headtext:
+            print(headtext['X-Request-Id'])
