51c2a4c8cdda82916fb11c5e5fbce934f7f90563
O <olivier.guyot@holbertonschool.fr>
2021-04-09 11:54:01 +0000

    fix

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import urllib.request as ur
+""" Module """
+
+if __name__ == "__main__":
+    with ur.urlopen("https://intranet.hbtn.io/status") as resp:
+        print(resp.getcode())
