e3de0781ae0fdc63eeff7bf7004167d2f559b2ce
O <olivier.guyot@holbertonschool.fr>
2021-04-09 13:42:05 +0000

    fix

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" Fetches """
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    url = argv[1]
+    resp = requests.get(url)
+    print(resp.headers['x-request-id'])
