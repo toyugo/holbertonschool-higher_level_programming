5acc45d56f25edeca732da9bfadb20d806294f4b
O <olivier.guyot@holbertonschool.fr>
2021-04-09 12:46:33 +0000

    fix

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """ Module  to get url response """
 import urllib.request
-import urllib.parse
 import sys
 
 if __name__ == "__main__":
+    url = sys.argv[1]
     data = urllib.parse.urlencode({"email": sys.argv[2]})
     data = data.encode('ascii')
-    url = sys.argv[1]
-    with open(urllib.request.urlopen(url, data)):
-        print("Your email is : {}".format(response.info['email']))
+    with urllib.request.urlopen(sys.argv[1], data) as resp:
+        a = resp.read()
+    print(a.decode('utf-8'))
+    
