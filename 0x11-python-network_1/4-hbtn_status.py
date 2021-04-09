9924467a3c1c9622ee22962cfb9a6d9a4767433b
O <olivier.guyot@holbertonschool.fr>
2021-04-09 13:40:32 +0000

    fix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" Fetches """
+import requests
+
+
+if __name__ == '__main__':
+    url = 'https://intranet.hbtn.io/status'
+    resp = requests.get(url)
+    print('Body response:')
+    print('\t- type: {}'.format(type(resp.text)))
+    print('\t- content: {}'.format(resp.text))
