9d0212e3ee53558597727ea10af6abc279d0de82
o <o@MacBookPro-B8E85644050A.home>
2021-05-04 23:37:26 +0200

    push

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,29 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+const req = request(url, (error, response, body) => {
+  const data = JSON.parse(body);
+  const obj = {};
+
+  for (const idxTask in data) {
+    let nbCompletedTask = 0;
+    if (data[idxTask].completed === true) {
+      nbCompletedTask++;
+    }
+    const userId = data[idxTask].userId;
+    if (!(obj[userId])) {
+      obj[userId] = 0;
+    }
+    obj[userId] += nbCompletedTask;
+  }
+  console.log(obj);
+
+  if (error) {
+    console.log(error);
+  }
+});
+req.on('error', error => {
+  console.error(error);
+});
+req.end();
