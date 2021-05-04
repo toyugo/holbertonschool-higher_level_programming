36d8dc237d0e026bf1571ef9e49ad0af29ac219a
o <o@MacBookPro-B8E85644050A.home>
2021-05-04 21:47:16 +0200

    Status Code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const https = require('https');
+const url = process.argv[2];
+
+const req = https.request(url, res => {
+  console.log(`Code: ${res.statusCode}`);
+});
+
+req.on('error', error => {
+  console.error(error);
+});
+req.end();
