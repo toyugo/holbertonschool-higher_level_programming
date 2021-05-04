b0c1edb47c25c159094ea9ef4597363c7129ed32
o <o@MacBookPro-B8E85644050A.home>
2021-05-04 22:48:59 +0200

    push

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/';
+
+const req = request(url, (error, response, body) => {
+  let cp = 0;
+  const data = JSON.parse(body).results;
+  for (const idMovie in data) {
+    const movieUrl = data[idMovie].characters;
+    for (const idxCharacter in movieUrl) {
+      if (movieUrl[idxCharacter].includes('18')) {
+        cp++;
+      }
+    }
+  }
+  console.log(cp);
+  if (error) {
+    console.log(error);
+  }
+});
+
+req.on('error', error => {
+  console.error(error);
+});
+req.end();
