1126476a41a39ee261a660110eee83a3f22469f0
o <o@MacBookPro-B8E85644050A.home>
2021-05-05 00:01:56 +0200

    push

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+const request = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + id;
+
+function getCharName (url) {
+  request(url, (error, response, body) => {
+    const data = JSON.parse(body);
+    console.log(data.name);
+    if (error) {
+      console.log(error);
+    }
+  });
+}
+
+request(url, (error, response, body) => {
+  const data = JSON.parse(body);
+  for (const idxCharUrl in data.characters) {
+    const urlChar = data.characters[idxCharUrl];
+    getCharName(urlChar);
+  }
+  if (error) {
+    console.log(error);
+  }
+});
