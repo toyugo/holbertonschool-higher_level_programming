ea7591440f3938b8ea99c3897555a173e1d7834e
o <o@MacBookPro-B8E85644050A.home>
2021-05-04 22:10:52 +0200

    push

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+const request = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + id;
+
+const req = request(url, (error, response, body) => {
+  content = 
+  console.log(`code: ${response.content}`);
+  if (error) {
+    console.log(error);
+  }
+});
+
+req.on('error', error => {
+  console.error(error);
+});
+req.end();
