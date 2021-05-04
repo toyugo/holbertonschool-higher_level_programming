8240fa5d9e346760a0a31eac317ecd86648fa341
o <o@MacBookPro-B8E85644050A.home>
2021-05-04 22:59:49 +0200

    push

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const fileName = process.argv[3];
+let content = '';
+request(url, (error, response, body) => {
+  content = body;
+  fs.writeFile(fileName, content, err => {
+    if (err) {
+      console.error(err);
+      return (err);
+    }
+  });
+  if (error) {
+    console.log(error);
+  }
+});
