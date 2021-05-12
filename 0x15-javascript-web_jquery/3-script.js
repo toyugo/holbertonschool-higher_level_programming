79d4fc900aac3d6480d0f5edb9c9631784eeea6c
toyugo <toyugo>
2021-05-12 16:04:32 +0000

    exo 4

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -3,6 +3,5 @@ $(document).ready(() => {
   $('DIV#red_header').click(function(){
     const header = $('header');
     header.addClass( "red");
-    header.css('color', '#FF0000');
-  });
+    });
 });
